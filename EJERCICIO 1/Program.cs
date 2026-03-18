b54b19ca78b9d5065f00addfd8fc3bcfaf43e494
Oscar Tahual <borussiadortmundyes@gmail.com>
2026-03-17 19:05:56 -0600

    Agregar archivos de proyecto.

---
+++ EJERCICIO 1/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+List<Estudiante> estudiantes = new List<Estudiante>();
+Console.WriteLine("Cuantos estudiantes desea ingresar ?");
+int num = int.Parse(Console.ReadLine());
+for (int i =0; i< num; i++)
+{
+    Estudiante e = new Estudiante();
+    Console.WriteLine($"Ingrese datos del estudiante {i+1}");
+    Console.WriteLine($"\nIngrese nombre del estudiante {i + 1}");e.Nombre = Console.ReadLine();
+    Console.WriteLine($" escriba nota 1 : ");e.Nota1 = double.Parse(Console.ReadLine());
+    Console.WriteLine($" escriba nota 2 : "); e.Nota2 = double.Parse(Console.ReadLine());
+    Console.WriteLine($" escriba nota 3 : "); e.Nota3 = double.Parse(Console.ReadLine());
+estudiantes .Add(e);
+}
+Estudiante mejorrendimiento = estudiantes[0];
+double sumadepromedios=0;
+Console.WriteLine("\nDatos regisstrados:");
+foreach (Estudiante d in estudiantes)
+{
+    d.MostrarDatosEtudiantes();
+    sumadepromedios =+ d.NotaPromedio();
+    if (d.NotaPromedio()> mejorrendimiento.NotaPromedio()) { mejorrendimiento = d; }
+}
+Console.WriteLine($"el promedio general es : {sumadepromedios:F2}");
+Console.WriteLine($"el mejor promedio es: {mejorrendimiento.NotaPromedio()} y pertenece a : {mejorrendimiento.Nombre}");
+class Estudiante
+{
+    public string Nombre;
+    public double Nota1;
+    public double Nota2;
+    public double Nota3;
+    public double NotaPromedio()
+    {
+        return (Nota1 + Nota2 + Nota3) / 3;
+    }
+    public string Aprobado()
+    {
+        if (NotaPromedio() < 60) return "reprobado";
+        else return "aprobado";
+    }
+    public void MostrarDatosEtudiantes()
+    {
+        Console.WriteLine($" Nombre del estudiante: {Nombre}   | \nNota primer semestre: {Nota1} | \nNota segundo  semestre: {Nota2} |\nNota tercer semestre: {Nota3}| \nPromedio: {NotaPromedio():F2}  ");
+    }
+}
+
---
+++ EJERCICIO EJEMPLO/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+List<DispositivosELectronico> dispositivo = new List<DispositivosELectronico>();
+Console.WriteLine("Cuantos dispositivos desea agregar : ");
+int n = int.Parse(Console.ReadLine());
+for (int i = 0; i < n; i++)
+{
+    DispositivosELectronico D = new DispositivosELectronico();
+    Console.WriteLine($" dispositivo {i + 1}: ");
+    Console.WriteLine($" escriba nombre del dispositivo: "); D.NombreDeldispositivo = Console.ReadLine();
+    Console.WriteLine($" escriba rendimiento 1 del dispositivo: "); D.Rendimiento1 = double.Parse(Console.ReadLine());
+    Console.WriteLine($" escriba rendimiento 2 del dispositivo: "); D.Rendimiento2 = double.Parse(Console.ReadLine());
+    Console.WriteLine($" escriba rendimiento 3 del dispositivo: "); D.Rendimiento3 = double.Parse(Console.ReadLine());
+    dispositivo.Add(D);
+}
+
+Console.WriteLine("\n datos de los dispositivos: ");
+double sumadepromedios = 0;
+DispositivosELectronico mejorrendimiento = dispositivo[0];
+foreach (DispositivosELectronico i in dispositivo)
+{
+    i.MostrarDatos();
+    sumadepromedios += i.CalcularPromedio();
+    if (i.CalcularPromedio() > mejorrendimiento.CalcularPromedio()) { mejorrendimiento = i; }
+
+
+}
+Console.WriteLine($"\n \t \t el promdio general de los dispositovos igresados son : {sumadepromedios:F2}");
+Console.WriteLine($"\n \t \t mejor promedio ");
+mejorrendimiento.MostrarDatos();
+class DispositivosELectronico
+{
+    public string NombreDeldispositivo;
+    public double Rendimiento1;
+    public double Rendimiento2;
+    public double Rendimiento3;
+    public double CalcularPromedio()
+    {
+        return (Rendimiento1 + Rendimiento2 + Rendimiento3) / 3;
+    }
+    public string ObtenerEstado()
+    {
+        if (CalcularPromedio() >= 80) return "Estado optimo";
+        else return "Necesita mantenimiento";
+    }
+    public void MostrarDatos()
+    {
+        Console.WriteLine($"Nombre: {NombreDeldispositivo} | Promedio: {CalcularPromedio():F2} | Estado: {ObtenerEstado():F2}");
+    }
+}
